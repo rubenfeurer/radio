9d95103e255ab5852b909e6006637d4f41902615
“rubenfeurer” <mail@rubenfeurer.com>
2024-11-16 14:37:46 +0100

    fix: delete wrong files

--- internetRadio-1.3.0-wifi-manager/src/__init__.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-ROOT_DIR = Path(__file__).parent.parent
-CONFIG_DIR = ROOT_DIR / 'config'
-LOGS_DIR = ROOT_DIR / 'logs'
--- internetRadio-1.3.0-wifi-manager/test_config.py
+++
@@ -1,6 +0,0 @@
-from src.utils.config_manager import ConfigManager
-import os
-
-config = ConfigManager()
-print("Config dir:", config.config_dir)
-print("Network config:", config.get_network_config())
