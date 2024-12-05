d5b2a2fa023933c8fd0e3579662bf2534789ab86
“rubenfeurer” <mail@rubenfeurer.com>
2024-12-05 09:06:42 +0100

    feat: setup test page

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { Button } from 'flowbite-svelte';
+</script>
+
+<div class="container mx-auto p-4">
+  <h1 class="text-2xl font-bold mb-4">Internet Radio</h1>
+  <Button>Test Button</Button>
+</div>
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -11,7 +11,6 @@ export default {
   theme: {
     extend: {
       colors: {
-        // flowbite-svelte
         primary: {
           50: '#FFF5F2',
           100: '#FFF1EE',
